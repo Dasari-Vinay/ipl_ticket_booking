e17e4c5a3eee446ea768a4d0ca2a35d991408984
Dasari-Vinay <vinay9885326500@gmail.com>
2026-09-25 22:39:54 +0530

    Delete ipl-ticket-booking directory4

--- ipl-ticket-booking/src/supabase.js
+++
@@ -1,9 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-
-const supabaseUrl = "https://oknrinupmubsfsgmqdub.supabase.co";
-const supabaseKey = "sb_publishable_qi-gEjGmI7xwSuLdtmKGHA_tRVFPDGf";
-
-export const supabase = createClient(
-    supabaseUrl,
-    supabaseKey
-);
